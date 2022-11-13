5ec6fe8a27edba38e482b0b220b71c23266aa8dd
QuangChau <chauptit98@gmail.com>
2022-11-13 21:08:58 +0700

    commit

--- js/main.js
+++ js/main.js
@@ -205,8 +205,8 @@ var ParticlePool = (function () {
 
     function pointOnHeart(t) {
         return new Point(
-            160 * Math.pow(Math.sin(t), 3),
-            130 * Math.cos(t) - 50 * Math.cos(2 * t) - 20 * Math.cos(3 * t) - 10 * Math.cos(4 * t) + 25
+            120 * Math.pow(Math.sin(t), 3),
+            90 * Math.cos(t) - 35 * Math.cos(2 * t) - 20 * Math.cos(3 * t) - 10 * Math.cos(4 * t) + 25
         );
     }
 
@@ -266,13 +266,6 @@ var ParticlePool = (function () {
         particles.draw(context, image);
     }
 
-    function renderText() {
-        console.log(canvas, context)
-        context.fillStyle = "white";
-        context.font = "bold 18px Arial";
-        context.fillText('aoiwhfaouwhf', (canvas.width / 2), (canvas.height / 2))
-    }
-
     // handle (re-)sizing of the canvas
     function onResize() {
         canvas.width = canvas.clientWidth;
@@ -284,7 +277,6 @@ var ParticlePool = (function () {
     setTimeout(function () {
         onResize();
         render();
-        renderText();
     }, 10);
 
 })(document.getElementById('heart'));
